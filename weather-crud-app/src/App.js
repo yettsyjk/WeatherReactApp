b023120eb5c39f77765b4595939bb19fa3144620
Matt Welbourn <mlwelbourn@gmail.com>
2020-01-27 15:18:53 -0700

    commit

--- weather-crud-app/src/App.js
+++ weather-crud-app/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import LoginRegisterForm from './LoginRegisterForm';
+import CityContainer from './CityContainer';
+import Header from './Header';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+const My404 = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        You are Lost
+      </div>
+    )
+};
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loggedIn: false,
+      loggedInUserEmail: null,
+    }
+  }
+
+  handleLoggedInStatus = (loggedInUserEmail) => {
+    this.setState({
+      loggedIn: true,
+      loggedInUserEmail: loggedInUserEmail
+    })
+  }
+
+  logout = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/logout`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const parsedLogoutResponse = await response.json();
+
+    if(parsedLogoutResponse.status.code === 200) {
+        this.setState({
+            loggedIn: false,
+            loggedInUserEmail: ''
+        })
+    } else {
+        console.log('Register Failed: ', parsedLogoutResponse);
+    }
+}
+
+render () {
+  return(
+    <main>
+      <Switch>
+        <Route exact path="/" render={(props) =>  <LoginRegisterForm {...props} loggedIn={this.state.loggedIn} loggedStatus={this.handleLoggedInStatus} /> } />
+        <Route exact path="/cities" render={(props) =>  <CityContainer {...props} loggedIn={this.state.loggedIn} loggedStatus={this.handleLoggedInStatus}  /> } />
+        <Route component={ My404 } />
+      </Switch>
+    </main>
+    )
+  }
 }
 
 export default App;
